64f017011aa47aa08248519c6c93dd334849e8ca
Alejandra Moreno <aleja_5313@hotmail.com>
2026-05-16 22:11:51 -0500

    Setting Render

--- andrea_beauty/andrea_beauty/settings.py
+++ andrea_beauty/andrea_beauty/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nb4pbmy^xbi7rz2jk-!c2-r7m8s!4uozv$!2fe-1231c93d%30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com',
+]
 
 
 # Application definition
@@ -115,4 +117,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
